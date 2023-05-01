a6ecdd3d77bacff4b11ad52388173bb999cb77a1
Amiroffir <amirosoffir@gmail.com>
2023-05-01 10:59:13 +0300

    to-ts init

--- Client/src/index.js
+++ Client/src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Domain } from "./global";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0Provider
-        domain="dev-ikv1ghu4jj70xyif.us.auth0.com"
+        domain={Auth0Domain}
         clientId="drOe6IzOIDzJ1iZbFV2YMefIHCVKdVZP"
         redirectUri={window.location.origin}
       >
