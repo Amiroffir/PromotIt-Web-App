17fa3e547b0e2db7491e76fd93d3553c2c31dd26
Amiroffir <amirosoffir@gmail.com>
2023-05-04 14:51:46 +0300

    to-ts

--- Client/src/contexts/contexts.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const RoleContext = createContext("");
-
-export const walletContext = createContext(0);
-
-export const identifiedUserContext = createContext(false);
